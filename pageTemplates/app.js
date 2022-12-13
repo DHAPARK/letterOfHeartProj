b3ed3e1fed3a915389f0f9c765177997b0744868
DHAPARK <pdh4182@gmail.com>
2022-12-13 11:14:46 +0900

    퍼블릭으로 모바일환경 체크

--- pageTemplates/app.js
+++ pageTemplates/app.js
@@ -7,7 +7,9 @@ app.use(express.static(__dirname + "/images"));
 app.get("/", (req, res) => {
   res.send(fs.readFileSync("./index.html", "utf-8"));
 });
-
+app.get("/d", (req, res) => {
+  res.send(fs.readFileSync("./design.html", "utf-8"));
+});
 app.listen(443, () => {
   console.log(`443번 포트로 서버 대기중..`);
 });
