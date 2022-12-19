9016660395c2881585583d42fe2de8a8ebb01887
DHAPARK <pdh4182@gmail.com>
2022-12-19 17:42:33 +0900

    정리

--- letterOfHeart/src/main/java/com/project/letterOfHeart/controller/SENSVerifyController.java
+++ letterOfHeart/src/main/java/com/project/letterOfHeart/controller/SENSVerifyController.java
@@ -4,29 +4,15 @@ import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.text.ParseException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.boot.autoconfigure.cassandra.CassandraProperties.Request;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.project.letterOfHeart.domain.Users;
-import com.project.letterOfHeart.dto.LoginForm;
 import com.project.letterOfHeart.dto.MessagesRequestDto;
 import com.project.letterOfHeart.dto.SendSmsResponseDto;
 import com.project.letterOfHeart.service.SmsService;
-import com.project.letterOfHeart.service.UsersService;
-import com.project.letterOfHeart.session.SessionConst;
 
 import lombok.RequiredArgsConstructor;
 
@@ -37,10 +23,16 @@ public class SENSVerifyController {
 	private final SmsService smsService;
 
 	// pdh
-	@PostMapping("/message/code")
-	public SendSmsResponseDto sendSms(@RequestBody MessagesRequestDto messageDto) throws UnsupportedEncodingException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+	//@PostMapping("/message/code")
+	@GetMapping("/message/code")
+	public String sendSms(@RequestBody MessagesRequestDto messageDto) throws UnsupportedEncodingException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+		System.out.println(" abcdefgabcdefg ") ;
+		System.out.println(" abcdefgabcdefg ") ;
         SendSmsResponseDto responseDto = smsService.sendSms(messageDto);
-        return responseDto;
+        
+        
+        //return responseDto;
+        return "suc";
     }
 
 	// 로그인
