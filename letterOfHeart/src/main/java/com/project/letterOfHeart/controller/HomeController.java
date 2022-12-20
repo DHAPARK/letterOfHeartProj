499548be26c6b6791914e098cdc52bd58622342a
DHAPARK <pdh4182@gmail.com>
2022-12-20 15:21:55 +0900

    최신화

--- letterOfHeart/src/main/java/com/project/letterOfHeart/controller/HomeController.java
+++
@@ -1,8 +0,0 @@
-package com.project.letterOfHeart.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class HomeController {
-
-}
--- letterOfHeart/src/main/java/com/project/letterOfHeart/domain/MessageStatus.java
+++
@@ -1,5 +0,0 @@
-package com.project.letterOfHeart.domain;
-
-public enum MessageStatus {
-	회원 ,비회원
-}
