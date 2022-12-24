dab8a0900ca887b3729a68e649d5ad93287f7650
vanny0421 <vannykim21@hotmail.com>
2022-12-24 20:56:36 +0900

    signup.js 수정

--- letterOfHeart/src/main/resources/static/js/user/signup.js
+++ letterOfHeart/src/main/resources/static/js/user/signup.js
@@ -2,24 +2,22 @@
  * Naver SENS 번호 인증
  */
  
-function regist(){
 
+window.addEventListener("load", function(event){
+	 
 	var phone = document.getElementById("phone");
     var numberck = document.getElementById("numberck");
     var authnumber = document.getElementById("authnumber");
     var smsck = document.getElementById("smsck");
     var numbermsg = document.getElementById("numbermsg");
-    var form = document.getElementById("signUpForm");
     var correctnumber = null;
     
 	numberck.addEventListener("click", function(){
         if(phone.value.trim().length<1){
             numbermsg.innerHTML = "전화번호는 필수 입력입니다.";
-            Flag = false;
             return;
         }else{
             numbermsg.innerText = "";
-            Flag = true;
         }
 
         $.ajax({
@@ -33,11 +31,9 @@ function regist(){
                 //alert(JSON.stringify(map.result));
                 if(map.result == "-1") { //중복 & 전송 실패
                     smsauth = false;
-                    Flag = false;
                     numbermsg.innerHTML = map.message;
                 }else{
                     smsauth = true;
-                    Flag = true;
                     numbermsg.innerText = map.message;
                     correctnumber = map.random;
                     $("#phone").prop("type", "hidden");
@@ -58,17 +54,19 @@ function regist(){
         if(authnumber.value==correctnumber){
             numbermsg.innerHTML = "인증되었습니다.</br>";
             smsauth = true;
-            Flag = true;
         }else{
             numbermsg.innerHTML = "인증에 실패하였습니다.</br>";
             smsauth = false;
-            Flag = false;
         }
     });
-    
-    if (confirm("핸드폰 번호를 검증하지 않으면 , 비밀번호 찾는데 이후에 문제가 생길 수 있습니다. 가입하시겠습니까?")) {
-		
+
+});
+
+
+function regist() {
+	var form = document.getElementById("signUpForm");
+	
+	if (confirm("핸드폰 번호를 검증하지 않으면 , 비밀번호 찾는데 이후에 문제가 생길 수 있습니다.")) {
         form.submit();
     } 
-    
 }
